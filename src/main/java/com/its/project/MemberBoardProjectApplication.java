307d1d1ede318c73c3d27eb313ab205f1b0abf4a
yunabi21 <yunabi21@gmail.com>
2022-06-28 15:21:26 +0900

    inital commit

---
+++ src/main/java/com/its/project/MemberBoardProjectApplication.java
@@ -0,0 +1,13 @@
+package com.its.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberBoardProjectApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MemberBoardProjectApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/its/project/MemberBoardProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.its.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberBoardProjectApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
